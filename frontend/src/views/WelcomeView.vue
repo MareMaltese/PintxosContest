d4687404283cb46f3af096a54baaee4ed04f810e
mare <mare.maltese@gmail.com>
2026-09-17 21:19:15 +0200

    Move background.png to app-wide body background, force light mode

--- frontend/src/views/WelcomeView.vue
+++ frontend/src/views/WelcomeView.vue
@@ -54,10 +54,6 @@ function goToRegister(): void {
   align-items: center;
   justify-content: center;
   padding: var(--space-5);
-  
-  background-image: url('../assets/background.png');
-  background-size: cover;
-  background-position: center;
 }
 
 .welcome__card {
